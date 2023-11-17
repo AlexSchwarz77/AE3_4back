d92ae90c155e5b3efe0160c75ee25a29bcbe7971
Alexander Schwarz <as@afp-akademie.de>
2023-11-17 15:05:50 +0100

    fixed issue #1

--- java_backend/src/main/java/de/afp/java_backend/JavaBackendApplication.java
+++ java_backend/src/main/java/de/afp/java_backend/JavaBackendApplication.java
@@ -2,7 +2,6 @@ package de.afp.java_backend;
 
 import java.util.Arrays;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
