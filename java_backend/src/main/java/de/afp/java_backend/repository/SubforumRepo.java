3d79202181dbdf7dc6c579f64f2c536ad097de70
Alexander Schwarz <schwarz@afpkoeln.onmicrosoft.com>
2023-11-24 09:37:30 +0100

    added change

--- java_backend/src/main/java/de/afp/java_backend/repository/SubforumRepo.java
+++ java_backend/src/main/java/de/afp/java_backend/repository/SubforumRepo.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import de.afp.java_backend.model.SubHelper;
+import de.afp.java_backend.model.ThreadHelper;
 import de.afp.java_backend.model.Subforum;
 import de.afp.java_backend.model.ThreadModel;
 
